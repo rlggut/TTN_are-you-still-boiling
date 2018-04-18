29acd2fa6d94765348eb2904bb4fe7cb2ac0b1b9
Роман <rlggut@mail.ru>
2018-04-18 23:46:30 +0300

    Add files via upload
    Arduino script

---
+++ Fading.ino
@@ -0,0 +1,36 @@
+const int buttonPin = 3;
+int buttonState = 0;         // переменная для хранения состояния кнопки
+int ledPin = 9;    // LED connected to digital pin 9
+
+void setup() {
+  pinMode(buttonPin, INPUT); 
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  int t=0;
+  if (buttonState == HIGH) {
+    while(t<20000)
+    {
+      analogWrite(ledPin, sin((3.14*t)/180)*122+123);
+      delay(30);
+      t++;
+    }
+    analogWrite(ledPin, 0);
+/*    while(t<20000)
+    {
+      analogWrite(ledPin, 255);
+      delay(30);
+      t++;
+    }
+    analogWrite(ledPin, 0);*/
+    /*while(t<20000)
+    {
+      analogWrite(ledPin, 255*(t%2));
+      delay(30);
+      t++;
+    }
+    analogWrite(ledPin, 0);*/
+  }
+}
+
